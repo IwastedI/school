71ebe1ca6446853ed1dbcb991e0da262a364a94e
Matthias De Nutte <75440772+IwastedI@users.noreply.github.com>
2024-09-22 12:16:58 +0200

    Add files via upload

---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mde-nutt <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/22 11:54:06 by mde-nutt          #+#    #+#             */
+/*   Updated: 2024/09/22 11:54:55 by mde-nutt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char value)
+{
+	write(1, &value, 1);
+}
+
+void	ft_display(int value)
+{
+	int	x;
+	int	y;
+
+	if (value > 9)
+	{
+		x = value / 10;
+		y = value % 10;
+		ft_putchar(x + '0');
+		ft_putchar(y + '0');
+	}
+	else
+	{
+		ft_putchar('0');
+		ft_putchar(value + 48);
+	}
+}
+
+void	ft_result(int x, int y)
+{
+	ft_display(x);
+	ft_putchar(' ');
+	ft_display(y);
+	if (x < 98 || y < 99)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb2(void)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	while (x <= 99)
+	{
+		y = x + 1;
+		while (y <= 99)
+		{
+			ft_result(x, y);
+			y++;
+		}
+		x++;
+	}
+}
